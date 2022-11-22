10cfd958ab8c85e1a22c0b6b86285aae2f17b754
Hanna Mandzirokha <hanna.mandzirokha@gmail.com>
2022-11-22 11:19:04 +0100

    added profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,25 @@
 import { Profile } from './Profile/Profile';
-import { UserInfo } from './UserInfo/UserInfo';
 import user from '../user.json';
 
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101',
+    // }}
     >
-      <Profile text="Profile" />
-      <UserInfo userinfo={user} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,41 @@
+import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
-export const Profile = ({ text }) => {
-  return <div className={css.profile}>{text}</div>;
+export const Profile = ({ avatar, username, tag, location, stats }) => {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
-//   <div className={css.profile}>
-//     <div className={css.description}>
-//       <img
-//         src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-//         alt="User avatar"
-//         className={css.avatar}
-//       />
-//       <p className={css.name}>Petra Marica</p>
-//       <p className={css.tag}>@pmarica</p>
-//       <p className={css.location}>Salvador, Brasil</p>
-//     </div>
-
-//     <ul className={css.stats}>
-//       <li>
-//         <span className={css.label}>Followers</span>
-//         <span className={css.quantity}>1000</span>
-//       </li>
-//       <li>
-//         <span className={css.label}>Views</span>
-//         <span className={css.quantity}>2000</span>
-//       </li>
-//       <li>
-//         <span className={css.label}>Likes</span>
-//         <span className={css.quantity}>3000</span>
-//       </li>
-//     </ul>
-//   </div>;
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
