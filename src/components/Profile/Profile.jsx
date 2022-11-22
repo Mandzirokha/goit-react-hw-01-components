897db3a57adf298b5a844397b5c0dc4d89b7e1e0
Hanna Mandzirokha <hanna.mandzirokha@gmail.com>
2022-11-22 22:00:39 +0100

    added styles for profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ export const Profile = ({ avatar, username, tag, location, stats }) => {
       <div className={css.description}>
         <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className={css.name}>{username}</p>
-        <p className={css.tag}>{tag}</p>
+        <p className={css.tag}>@{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
       <ul className={css.stats}>
